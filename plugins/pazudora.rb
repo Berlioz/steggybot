91f143c2a31068077e4271a8754e5242ca723ef9
Victor Fan <victor@verticalbrands.com>
2013-05-02 16:36:50 -0700

    some comedy features for #pad

--- plugins/pazudora.rb
+++ plugins/pazudora.rb
@@ -5,11 +5,11 @@ require 'open-uri'
 
 class Pazudora
   include Cinch::Plugin
-  
+
   PUZZLEMON_BASE_URL = "http://www.puzzledragonx.com/en/"
   GACHA_URL = "http://www.puzzledragonx.com/egg.asp"
   RANK_TABLE_URL = "http://www.puzzledragonx.com/en/levelchart.asp"
-  
+
   match /pazudora ([\w-]+) *(.+)*/i, method: :pazudora
   match /stupidpuzzledragonbullshit ([\w-]+) *(.+)*/i, method: :pazudora
   match /stupiddragonpuzzlebullshit ([\w-]+) *(.+)*/i, method: :pazudora
@@ -17,47 +17,47 @@ class Pazudora
   match /pad ([\w-]+) *(.+)*/i, method: :pazudora
   match /puzzlemon ([\w-]+) *(.+)*/i, method: :pazudora
   match /puzzledex (.+)*/i, method: :pazudora_lookup
-  
+
   @help_string = <<-eos
   Puzzle and Dragons (on iOS and Android) utility functions. Mostly collects friend codes and tracks the daily dungeon.
   Its commands are aliased to pazudora, p&d, pad, and puzzlemon.
   eos
   @help_hash = {
-    :add => "Usage: !puzzlemon add NAME CODE
+      :add => "Usage: !puzzlemon add NAME CODE
 Example: !puzzlemon add steggybot 123,456,789
 Adds user's code to the list of friend codes",
-    :group => "Usage: !puzzlemon group NAME
+      :group => "Usage: !puzzlemon group NAME
 Example !puzzlemon group steggybot
 Returns the daily dungeon group that the user belongs to.",
-    :dailies => "Usage: !puzzlemon dailies
+      :dailies => "Usage: !puzzlemon dailies
 Example !puzzlemon dailies
 Returns the times (and type) of today's daily dungeons, split by group. If called by
 a registered player, also reminds them of their group id. Buggy.",
-    :lookup => "Usage: !(puzzlemon lookup|puzzledex) (NAME|ID)
+      :lookup => "Usage: !(puzzlemon lookup|puzzledex) (NAME|ID)
 Example !puzzlemon lookup horus, !puzzledex 603
 Returns a description of the puzzlemon. Aliases: dex",
-    :list => "Usage: !puzzlemon list
+      :list => "Usage: !puzzlemon list
 Example !puzzlemon list
 Returns a list of everyone's friends codes.",
-    :chain => "Usage: !puzzlemon chain (NAME|ID)
+      :chain => "Usage: !puzzlemon chain (NAME|ID)
 Example !puzzlemon chain siren
 Returns a sumary of the given puzzlemon's evolutionary family.",
-    :evolution => "Usage: !puzzlemon evolution
+      :evolution => "Usage: !puzzlemon evolution
 Example !puzzlemon evolution siren
 Returns the set of evo materials necessary to advance to the next tier. Aliases: materials, mats, evolve",
-    :experience => "Usage: !puzzlemon experience [FROM]
+      :experience => "Usage: !puzzlemon experience [FROM]
 Example !puzzlemon siren 24
 Calculates the amount of experience required to max out this puzzlemon. From defaults to 1.
 Aliases: exp, level. YES I KNOW THERE ARE NO LIGHT/DARK PENGDRAS A BLOOO BOO HOO",
-   :rank => "Usage: !puzzlemon rank (RANK|TO FROM)
+      :rank => "Usage: !puzzlemon rank (RANK|TO FROM)
 Example !puzzlemon rank 60, !puzzlemon rank 100 120
 Returns information about a given player rank, or calculates the deltas between them.
 Reverse lookup also possible: !puzzlemon rank stamina 100 computes when you will get 100 stamina.",
-   :gacha => "Usage: !puzzlemon gacha
+      :gacha => "Usage: !puzzlemon gacha
 Simulates a pull from the rare egg machine. No, this doesn't take events or weighting into account and is in fact almost certainly horrifyingly wrong in every particular.
 User !puzzlemon gacha MONSTER to learn how many rolls you'll need to get that monster, e.g !pad gacha Horus
 Are you feeling lucky? This command can change that. Aliases: pull, roll",
-   :stamina => "Usage: !puzzlemon stamina START END TIMEZONE
+      :stamina => "Usage: !puzzlemon stamina START END TIMEZONE
 Computes how long it will take to go from START (default 0) to END stamina, and when it will happen in your timezone.
 Input your timezone as an integer UTC offset, e.g +7 or -11. Defaults to -7 (pacific standard)."
   }
@@ -65,24 +65,24 @@ Input your timezone as an integer UTC offset, e.g +7 or -11. Defaults to -7 (pac
   HELP = @help_hash
 
   ALIAS = {
-    "delete" => "remove",
-    "dex" => "lookup",
-    "evolve" => "evolution",
-    "materials" => "evolution",
-    "mats" => "evolution",
-    "xp" => "experience",
-    "exp" => "experience",
-    "level" => "experience",
-    "pull" => "gacha",
-    "roll" => "gacha",
-    "stam" => "stamina"
+      "delete" => "remove",
+      "dex" => "lookup",
+      "evolve" => "evolution",
+      "materials" => "evolution",
+      "mats" => "evolution",
+      "xp" => "experience",
+      "exp" => "experience",
+      "level" => "experience",
+      "pull" => "gacha",
+      "roll" => "gacha",
+      "stam" => "stamina"
   }
-  
+
   def initialize(*args)
     super
     @pddata = config[:pddata]
   end
-  
+
   #Any public method named pazudora_[something] is external and
   #can be accessed by the user using the construction "!puzzlemon something [args]".
   def pazudora (m, cmd, args)
@@ -111,17 +111,17 @@ Input your timezone as an integer UTC offset, e.g +7 or -11. Defaults to -7 (pac
       m.reply helpstr
     end
   end
-  
+
   def pazudora_add (m, args)
     pargs = args.split
     username = pargs[0]
     friend_code = pargs[1]
-    
+
     # add it to the list
     friend_codes = load_data || {}
     if friend_code =~ /[0-9]{9}/
       friend_code = friend_code.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse
-    end 
+    end
     if friend_codes[username.downcase] && (friend_codes[username.downcase][:added_by] == username.downcase)
       if m.user.nick.downcase == username.downcase
         friend_codes[username.downcase] = {:friend_code => friend_code, :added_by => m.user.nick.downcase, :updated_at => DateTime.now}
@@ -136,33 +136,33 @@ Input your timezone as an integer UTC offset, e.g +7 or -11. Defaults to -7 (pac
     output = File.new(@pddata, 'w')
     output.puts YAML.dump(friend_codes)
     output.close
-      
+
     m.reply "#{m.user.nick}: #{mangle(username)} successfully added as '#{friend_code}'."
   end
-  
+
   def pazudora_remove(m, args)
     pargs = args.split
     username = pargs[0]
     friend_codes = load_data || {}
-    
+
     if !friend_codes[username]
       m.reply "#{m.user.nick}: #{mangle(username)} not in friend code list."
       return
     end
-    
+
     if (m.user.nick.downcase == username.downcase) || (m.user.nick.downcase == friend_codes[username][:added_by])
       friend_codes.delete(username)
       # write it to the file
       output = File.new(@pddata, 'w')
       output.puts YAML.dump(friend_codes)
       output.close
-      
+
       m.reply "#{m.user.nick}: #{mangle(username)} removed from friend code list."
     else
       m.reply "#{m.user.nick}: Only the identity creator or subject can remove a friend code from the list."
     end
   end
-  
+
   def pazudora_who(m,args)
     pargs = args.split
     username = pargs[0]
@@ -170,16 +170,16 @@ Input your timezone as an integer UTC offset, e.g +7 or -11. Defaults to -7 (pac
       m.reply "#{m.user.nick}: #{mangle(username)}'s friend code is #{load_data[username.downcase][:friend_code]}"
     end
   end
-  
+
   def pazudora_list(m,args)
     friend_codes = load_data || {}
-    friend_codes.keys.each_slice(3).with_index { |users, i| 
+    friend_codes.keys.each_slice(3).with_index { |users, i|
       r = i == 0 ? "#{m.user.nick}: " : ""
       users.each{ |user| r=r+"#{mangle(user)}:#{friend_codes[user][:friend_code]} " }
       m.reply r
     }
   end
-  
+
   def pazudora_group(m,args)
     pargs = args.split
     username = pargs[0]
@@ -191,35 +191,35 @@ Input your timezone as an integer UTC offset, e.g +7 or -11. Defaults to -7 (pac
       m.reply "#{m.user.nick}: #{mangle(username)}'s friend code is not listed. Please use the add command to add it."
     end
   end
-  
+
   def pazudora_dailies(m, args)
     daily_url = PUZZLEMON_BASE_URL + "option.asp?utc=-8"
-    
+
     username = args.split[0] || m.user.nick
     friend_code = load_data[username.downcase][:friend_code] rescue nil
     group_num = friend_code ? group_number_from_friend_code(friend_code) : nil
-    
+
     unless @daily_timestamp == Time.now.strftime("%m-%d-%y")
       @daily_page = nil
       @daily_timestamp = Time.now.strftime("%m-%d-%y")
     end
-    
+
     @daily_page ||= Nokogiri::HTML(open(daily_url))
     event_data = @daily_page.css(".event3")
     event_rewards = @daily_page.css(".limiteddragon")
-    
+
     rewards = parse_daily_dungeon_rewards(event_rewards)
     m.reply "Dungeons today are: #{rewards.join(', ')}"
-    
+
     (0..4).each do |i|
       m.reply "Group #{(i + 65).chr}: #{event_data[i].text}, #{event_data[i + 5].text}, #{event_data[i + 10].text}"
     end
-    
+
     if group_num
       m.reply "User #{username} is in group #{(group_num + 65).chr}"
     end
   end
-  
+
   def pazudora_lookup(m, args)
     identifier = args
     pdx = PazudoraData.instance.get_puzzlemon(identifier)
@@ -243,11 +243,11 @@ Input your timezone as an integer UTC offset, e.g +7 or -11. Defaults to -7 (pac
 
   def pazudora_experience(m, args)
     argv = args.split(" ")
-		if is_numeric?(argv.last)
-	    from = argv.pop.to_i
+    if is_numeric?(argv.last)
+      from = argv.pop.to_i
     else
       from = 1
-	  end
+    end
     identifier = args
     pdx = PazudoraData.instance.get_puzzlemon(identifier)
     m.reply "Could not find puzzlemon #{identifier}" and return if pdx.nil?
@@ -305,8 +305,34 @@ Input your timezone as an integer UTC offset, e.g +7 or -11. Defaults to -7 (pac
       r = "Use +[tags] to denote godfest; for example !pad pull +JGO for a japanese/greek/odins fest.\n"
       r += "Known tags: [R]oman, [J]apanese, [H]indu, [N]orse, [E]gyptian, [G]reek, [O]dins"
       m.reply r
+    elsif is_numeric?(args)
+      gods = []
+      args.to_i.times do
+        pdx = PazudoraData.instance.gachapon(godfest_flags)
+        stars = pdx.stars
+        type = pdx.type
+        name = pdx.name
+        if stars >= 5 && type == "god" && !pdx.name.include?("Verche")
+          gods << pdx.name
+        end
+      end
+      overflow = 0
+      if gods.length > 10
+        overflow = gods.length - 10
+        gods = gods[0..9]
+      end
+      price = stone_price(args.to_i * 5)
+      if gods.length == 0
+        r = "You rolled #{args} times (for $#{price}) and got jackshit all. Gungtrolled."
+      else
+        r = "You rolled #{args} times (for $#{price}) and got some gods:\n"
+        r += gods.join(", ")
+        if overflow > 0
+          r += "...and #{overflow} more"
+        end
+      end
+      m.reply r
     elsif args == ""
-      p godfest_flags
       pdx = PazudoraData.instance.gachapon(godfest_flags)
       stars = pdx.stars
       type = pdx.type
@@ -345,7 +371,8 @@ Input your timezone as an integer UTC offset, e.g +7 or -11. Defaults to -7 (pac
         break if regex && (pdx.name.downcase.match(identifier) || pdx.name.match(identifier))
         m.reply("Unable to roll #{identifier}") and return if attempts == 10000
       end
-      m.reply("After #{attempts} attempts, you rolled a #{pdx.name}. (There goes $#{attempts * 5})")
+      price = stone_price(attempts * 5)
+      m.reply("After #{attempts} attempts, you rolled a #{pdx.name}. (There goes $#{price})")
     end
   end
 
@@ -390,7 +417,7 @@ Input your timezone as an integer UTC offset, e.g +7 or -11. Defaults to -7 (pac
     end
     m.reply(r)
   end
-  
+
   protected
   def is_numeric?(str)
     begin
@@ -408,6 +435,17 @@ Input your timezone as an integer UTC offset, e.g +7 or -11. Defaults to -7 (pac
     end
   end
 
+  def stone_price(stones)
+    prices = {1 => 1, 6 => 5, 12 => 10, 30 => 23, 60 => 44, 85 => 60}
+    money = 0
+    while stones > 0
+      selection = prices.keys.select{|x| x <= stones}.max
+      stones = stones - selection
+      money = money + prices[selection]
+    end
+    money
+  end
+
   def rank_data
     ranks = Nokogiri::HTML.parse(open(RANK_TABLE_URL).read)
     rows = ranks.xpath("//table[@id='tablechart']").first.children
@@ -421,7 +459,7 @@ Input your timezone as an integer UTC offset, e.g +7 or -11. Defaults to -7 (pac
       exp_total = cells[4].children.to_s
       exp_next = cells[5].children.to_s
       rv[level] = {cost:cost,
-                   stamina:stamina, 
+                   stamina:stamina,
                    friends:friends,
                    exp_total:exp_total,
                    exp_next:exp_next}
@@ -442,28 +480,28 @@ Input your timezone as an integer UTC offset, e.g +7 or -11. Defaults to -7 (pac
     end
     out.strip
   end
-  
+
   def load_data
     datafile = File.new(@pddata, 'r')
     friend_codes = YAML.load(datafile.read)
     datafile.close
     return friend_codes
   end
-  
+
   def group_number_from_friend_code(friend_code)
     return Integer(friend_code.split(",")[0][2]) % 5
   end
-  
+
   def parse_daily_dungeon_rewards(rewards)
     puzzlemon_numbers = [
-    rewards[0].children.first.attributes["src"].value.match(/thumbnail\/(\d+).png/)[1],
-    rewards[5].children.first.attributes["src"].value.match(/thumbnail\/(\d+).png/)[1],
-    rewards[10].children.first.attributes["src"].value.match(/thumbnail\/(\d+).png/)[1]]
-    
-    puzzlemon_numbers.map{|x| 
+        rewards[0].children.first.attributes["src"].value.match(/thumbnail\/(\d+).png/)[1],
+        rewards[5].children.first.attributes["src"].value.match(/thumbnail\/(\d+).png/)[1],
+        rewards[10].children.first.attributes["src"].value.match(/thumbnail\/(\d+).png/)[1]]
+
+    puzzlemon_numbers.map{|x|
       get_puzzlemon_info(x).css(".name").children.first.text rescue "Unrecognized Name" }
   end
-  
+
   def get_puzzlemon_info(name_or_number)
     search_url = PUZZLEMON_BASE_URL + "monster.asp?n=#{name_or_number}"
     puzzlemon_info = Nokogiri::HTML(open(search_url))
